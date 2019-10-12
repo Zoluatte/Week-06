dc664e9ee4bd8134877038d579606c7977b1a291
“Saya” <61030238@kmitl.ac.th>
2019-10-12 19:26:47 +0700

    sayHello() error

--- 61030238/ConsoleApp1/ConsoleApp1/Program.cs
+++ 61030238/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Net;  // for web client
-using System.IO;   // for stream writer
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,17 +10,12 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            // Create web client.
-            WebClient client = new WebClient();
-            // Download string.
-            string value = client.DownloadString("http://www.kmitl.ac.th");
-            // Write values.
-            Console.WriteLine("--- WebClient result ---");
-            Console.WriteLine(value.Length);
-            Console.WriteLine(value);
-            StreamWriter outtxt = new StreamWriter("webload.html");
-            outtext.Write(value);
-            outtxt.Dispose();
+            sayHello();
+            Console.ReadLine();
+        }
+        void sayHello()
+        {
+            Console.WriteLine("Hello World.");
         }
     }
 }
