e3ae0c0946f7389e2299b91443aba6823d34055e
Banana1600 <nakorn.tao16@gmail.com>
2019-10-12 12:22:05 +0700

    7

---
+++ week-06/week-06/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        object[] myObjArray = { 2, 'b', 1000.05d, "Hello World" };
+        useParams(myObjArray);
+        useParams("IE.Tech.", "KMITL", 2017);
+        Console.ReadLine();
+    }
+
+    static void useParams(params object[] list)
+    {
+        foreach (object o in list)
+            Console.Write(o + " ");
+        Console.WriteLine();
+    }
+}
