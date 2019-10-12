e4b70b6eb0df69e3907a9633be55c317d7f7ede3
Banana1600 <nakorn.tao16@gmail.com>
2019-10-12 14:59:49 +0700

    ข้อ 1

--- week-06/week-06/Program.cs
+++ week-06/week-06/Program.cs
@@ -3,11 +3,29 @@
 class Program
 {
     static void Main()
+
     {
-        int a = 2;
-        float f = 3.5f;
-        double d = 4.99d;
-        decimal money = 1254841354;
-        Console.WriteLine(a + " " + f + " " + d + " " + money);
+        int number;
+
+        Console.WriteLine("Enter Interger Number : ");
+
+        number = int.Parse(Console.ReadLine());
+        Banana(number);
+
+        Console.ReadLine();
+
+    }
+
+    static void Banana(int number)
+    {
+        if ((number % 2) == 0)
+        {
+
+            Console.WriteLine("{0} Is Even", number);
+        }
+        else
+        {
+            Console.WriteLine("{0} Is Odd", number);
+        }
     }
 }
