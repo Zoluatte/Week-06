d45c666114cc76b6a16f5039904457f87e649808
Banana1600 <nakorn.tao16@gmail.com>
2019-10-12 13:21:51 +0700

    20

--- week-06/week-06/Program.cs
+++ week-06/week-06/Program.cs
@@ -4,9 +4,8 @@ class Program
 {
     static void Main()
     {
-        double d = 5e9d;
-        Console.WriteLine(d);
-        int i = checked((int)d);
-        Console.WriteLine(i);
+        int a = 2;
+        string myString = a.ToString();
+        Console.WriteLine(myString);
     }
 }
