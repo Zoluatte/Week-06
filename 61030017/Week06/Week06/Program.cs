e007bfb76357a69d5eae41f525b4d9221efcbb0d
NapakorN <61030017@kmitl.ac.th>
2019-11-01 15:52:52 +0700

    Edited by napakorn 61030017

---
+++ 61030017/Week06/Week06/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        float weight;
+        Console.WriteLine("Input your weight");
+        weight = float.Parse(Console.ReadLine());
+        Console.WriteLine("Your weight on moon is : {0:F1}",calculate(ref weight));
+    }
+
+    static float calculate(ref float weight)
+    {
+        weight /= 10;
+        weight *= 1.6f;
+        return weight;
+    }
+}
