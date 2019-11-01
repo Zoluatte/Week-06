ce1517b319e37e1954598c47235cedd24f6bf785
Phruksa <phruksa4165@gmail.com>
2019-11-01 14:31:26 +0700

    add

--- 61030035/Lab6/Lab6/Program.cs
+++ 61030035/Lab6/Lab6/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,13 +12,16 @@ namespace Lab6
     {
         static void Main(string[] args)
         {
-            sayHello();
+            long c = Factorial(5);
+            Console.WriteLine("Factorial of 5 = {0}", c);
             Console.ReadLine();
         }
 
-        static void sayHello()
+        static long Factorial(int n)
         {
-            Console.WriteLine("Hello World.");
+            if (n == 0)  // Factorial 0 = 1
+                return 1;
+            return n * Factorial(n - 1);
 
         }
     }
