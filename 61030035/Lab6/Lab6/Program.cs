a16f13ae2f830412f1ef18a5bd858a4799d48e32
Phruksa <phruksa4165@gmail.com>
2019-11-01 13:49:45 +0700

    add

---
+++ 61030035/Lab6/Lab6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sayHello();
+            Console.ReadLine();
+        }
+        void sayHello()
+        {
+            Console.WriteLine("Hello World.");
+        
+    }
+    }
+}
