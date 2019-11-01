a408b465dd182b5cc697a6bb3f8e46a3780207aa
Tonkla <tonklazooza1@gmail.com>
2019-11-01 15:30:56 +0700

    week-06

---
+++ 61030007/week-06/week-06/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week_06
+{
+
+
+    class Program
+    {
+        static void Main()
+        {
+            for (int dec = 1; dec <= 100; dec++)
+            {
+                string bin = null;
+                int ans;
+                int num = dec;
+                int check = 0;
+
+                while (num >= 1)
+                {
+                    ans = num % 2;
+                    bin = ans.ToString() + bin;
+                    num /= 2;
+                }
+                if (bin.Length > 4)
+                {
+                    if (bin.Length > 5)
+                    {
+                        if (bin.Length > 6)
+                        {
+                            //Console.WriteLine($"Decimal  : {dec}\nBinary : {bin}");
+                            //Console.WriteLine(bin.Substring(2, 1));                            check = int.Parse(bin.Substring(2, 1));
+                            checkBit5(check, dec);
+                            //digit5[dec] = check;
+                            continue;
+                        }
+                        //Console.WriteLine($"Decimal  : {dec}\nBinary : {bin}");
+                        //Console.WriteLine(bin.Substring(1, 1));
+                        check = int.Parse(bin.Substring(1, 1));
+                        checkBit5(check, dec);
+                        //digit5[dec] = check;
+                        continue;
+                    }
+                    //Console.WriteLine($"Decimal  : {dec}\nBinary : {bin}");
+                    //Console.WriteLine(bin.Substring(0, 1));
+                    check = int.Parse(bin.Substring(0, 1));
+                    checkBit5(check, dec);
+
+                }
+            }
+        }
+            static void checkBit5(int bin, int dec)
+            {
+                if (bin == 1)
+                {
+                    Console.WriteLine($"Decimal  : {dec}  Binary : {bin}");
+                }
+            }
+        }
+    }
