42bb70b4410d1667c09c995a1a6bfdb5e604f87b
“Saya” <61030238@kmitl.ac.th>
2019-11-10 14:16:54 +0700

    5555

--- 61030238/ConsoleApp1/ConsoleApp1/Program.cs
+++ 61030238/ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,24 +8,35 @@ namespace ConsoleApp1
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            for (int t = 1; t <= 100; t++)
+            {
+                {
+                    String aa = "";
+                    while (t > 0)
+                    {
+                        if (t >= 2)
+                        {
+                            aa = (t % 2).ToString() + aa;
+                        }
+                        else
+                        {
+                            aa = t.ToString() + aa;
+                        }
+                        t = t / 2;
 
-            double a, c;
-            double v = 1 / 6;
-            Console.WriteLine("น้ำหนักเรา");
 
-            a = double.Parse(Console.ReadLine());
+                    }
 
-            c = add(a, v);
-            Console.WriteLine(c);
-        }
 
-        static double add(double a, double v)
-        {
-            return a * v;
+                    int n = int.Parse(aa); 
+                    Console.WriteLine(n);
+                }
+            }
         }
     }
-
 }
+
 
