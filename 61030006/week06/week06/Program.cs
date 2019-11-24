8f1538d79e514e4108f9e8f08b79ed2486344d53
kkkanitar <khanita03272@gmail.com>
2019-11-24 17:14:45 +0700

    hgfd

---
+++ 61030006/week06/week06/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s,l;
+            Console.WriteLine("enter number start : ");
+            s = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter number last : ");
+            l = int.Parse(Console.ReadLine());
+            Output(s,l);
+           
+        }
+
+        static void Output(int s,int l)
+        {
+            for (int n = s; n <= l; n++)
+            {
+                if (n % 7 == 0)
+                Console.WriteLine($"number : {n}");
+
+            }
+            
+        }
+    }
+}
