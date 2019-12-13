fc8c6f847701f7e9b8fbecf1ebe81791517aa7f4
earnearn <61030057@kmitl.ac.th>
2019-12-13 17:16:15 +0700

    add photo

---
+++ 61030057/LAB6/LAB6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            float f = 3.5f;
+            double d = 4.99d;
+            decimal money = 1254841354;
+            Console.WriteLine(a + " " + f + " " + d + " " + money);
+        }
+    }
+}
+   
+
